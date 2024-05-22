66993564ca7cf78771c467b27a525316f8587e2d
Sathwik <hsathwik002@gmail.com>
2024-05-22 22:01:00 +0530

    startingFile/meeting-DynamicRoute_File

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <Button variant="outline">Button</Button>
-
+    <Button variant="destructive">Destructive</Button>
   );
 }
