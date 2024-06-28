7e8c92667826fb0e0ef071b1e2b23d08ca49d37e
Sathwik <hsathwik002@gmail.com>
2024-06-28 18:29:01 +0530

    [updated]:RecordingsPage

--- components/CallList/CallList.tsx
+++ components/CallList/CallList.tsx
@@ -2,11 +2,13 @@
 
 import { Call, CallRecording } from '@stream-io/video-react-sdk';
 
-import Loader from './Loader';
+
 import { useGetCalls } from '@/hooks/useGetCalls';
-import MeetingCard from './MeetingCard';
+
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import Loader from '../Loder/Loder';
+import MeetingCard from '../MeetingCard/MeetingCard';
 
 const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
   const router = useRouter();
