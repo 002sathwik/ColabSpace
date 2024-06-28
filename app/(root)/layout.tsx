d634a58c9d6212665e05c5f1c85c53c1e773f729
Sathwik <hsathwik002@gmail.com>
2024-06-28 17:32:07 +0530

    [updated]:metadata

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,11 @@
 import StreamVideoProvider from "@/providers/StreamVideoClientProvider";
 import React, { ReactNode } from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "ColabSpace",
+  description: "Space for colabaration",
+};
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
